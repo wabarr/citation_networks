c393af6222809e36f24260e6ff7ee9d8b017bd77
wabarr <wabarr@gmail.com>
2022-10-29 14:14:04 -0400

    simplify code logic

--- citation_networks/forms.py
+++ citation_networks/forms.py
@@ -5,96 +5,90 @@ import json
 from citation_networks.models import Paper, Author, Authorship
 import datetime
 
+
+def add_authors(paper_object, JSON_authorlist):
+    ## given an instance of a Paper object, and a JSON list of authors from the SS API, create Authorship
+    authors = enumerate(JSON_authorlist)
+    for i, author in authors:
+        theAuthorObject, created = Author.objects.get_or_create(SS_author_ID=author["authorId"])
+        if created:
+            theAuthorObject.name = author["name"]
+            theAuthorObject.save()
+        numAuthors = len(JSON_authorlist)
+        if i == 0:
+            Authorship.objects.get_or_create(paper=paper_object,
+                                             author=theAuthorObject,
+                                             authorPosition="F")
+
+        elif i == numAuthors - 1:
+            Authorship.objects.get_or_create(paper=paper_object,
+                                             author=theAuthorObject,
+                                             authorPosition="L")
+        else:
+            Authorship.objects.get_or_create(paper=paper_object,
+                                             author=theAuthorObject,
+                                             authorPosition="I")
+
+def helper_add_cites_refs(paper_object, JSON_list, citationORreference):
+    #given a paper object, a list of citations or references, and a string indicating
+    #whether the papers in JSON_list are papers or refereneces, add all the papers to db
+    #this is used to avoid having nearly identifical code blocks for adding refs and cites
+    for work in JSON_list:
+        ## if no paperId try based on title
+        if not work["paperId"]:
+            potentially_new_paper, created = Paper.objects.get_or_create(title=work["title"])
+        else:
+            potentially_new_paper, created = Paper.objects.get_or_create(SSID_paper_ID=work["paperId"])
+        if created:
+            potentially_new_paper.title = work["title"]
+            try:
+                potentially_new_paper.journal_name = work["journal"]["name"]
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+            try:
+                potentially_new_paper.volume = work["journal"]["volume"]
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+            try:
+                potentially_new_paper.issue = work["journal"]["issue"]
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+            try:
+                potentially_new_paper.pages = work["journal"]["pages"]
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+            potentially_new_paper.year = work["year"]
+            potentially_new_paper.save()
+            add_authors(potentially_new_paper, work["authors"])
+
+        if citationORreference == "citation":
+            paper_object.cited_by.add(potentially_new_paper)
+            paper_object.save()
+
+        if citationORreference == "reference":
+            paper_object.references.add(potentially_new_paper)
+            paper_object.save()
+
+        paper_object.citations_last_queried = datetime.datetime.now().astimezone()
+
 class ImportCitations(forms.Form):
     ssid = forms.CharField(help_text="Enter Semantic Scholar ID")
-
-    def addAuthors(self, paper_object, JSON_authorlist):
-        authors = enumerate(JSON_authorlist)
-        for i, author in authors:
-            theAuthorObject, created = Author.objects.get_or_create(SS_author_ID=author["authorId"],
-                                                                    name=author["name"])
-            numAuthors = len(JSON_authorlist)
-            if i == 0:
-                Authorship.objects.get_or_create(paper=paper_object,
-                                                 author=theAuthorObject,
-                                                 authorPosition="F")
-
-            elif i == numAuthors - 1:
-                Authorship.objects.get_or_create(paper=paper_object,
-                                                 author=theAuthorObject,
-                                                 authorPosition="L")
-            else:
-                Authorship.objects.get_or_create(paper=paper_object,
-                                                 author=theAuthorObject,
-                                                 authorPosition="I")
 
     def addCitationsRefs(self, id):
         thePaperObject = Paper.objects.get(SSID_paper_ID=id)
         thePaperJSON = json.loads(thePaperObject.raw_SS_json)
-        for citation in thePaperJSON["citations"]:
-            citing_paper, created = Paper.objects.get_or_create(SSID_paper_ID=citation["paperId"])
-            citing_paper.title = citation["title"]
-            try:
-                citing_paper.journal_name = citation["journal"]["name"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                citing_paper.volume = citation["journal"]["volume"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                citing_paper.issue = citation["journal"]["issue"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                citing_paper.pages = citation["journal"]["pages"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            citing_paper.year = citation["year"]
-            citing_paper.save()
-            self.addAuthors(citing_paper, citation["authors"])
-            thePaperObject.cited_by.add(citing_paper)
-            thePaperObject.save()
-        for reference in thePaperJSON["references"]:
-            referring_paper, created = Paper.objects.get_or_create(SSID_paper_ID=reference["paperId"])
-            referring_paper.title = reference["title"]
-            try:
-                referring_paper.journal_name = reference["journal"]["name"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                referring_paper.volume = reference["journal"]["volume"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                referring_paper.issue = reference["journal"]["issue"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            try:
-                referring_paper.pages = reference["journal"]["pages"]
-            except KeyError:
-                pass
-            except TypeError:
-                pass
-            referring_paper.year = reference["year"]
-            referring_paper.save()
-            self.addAuthors(referring_paper, reference["authors"])
-            thePaperObject.references.add(referring_paper)
-            thePaperObject.save()
+        
+        helper_add_cites_refs(thePaperObject, thePaperJSON["citations"], "citation")
+
+        helper_add_cites_refs(thePaperObject, thePaperJSON["references"], "reference")
 
     def clean(self):
         super().clean()
@@ -102,15 +96,16 @@ class ImportCitations(forms.Form):
         ## raw json into a model field called Paper.raw_SS_json
         id = self.cleaned_data["ssid"]
         semantic_API_url = "https://api.semanticscholar.org/graph/v1/paper/"
-        status_code = None
+        #status_code = None
+        fields = ["citationCount", "authors", "year", "title", "journal", "publicationTypes", "abstract",
+                  "citations", "citations.authors", "citations.year", "citations.title",
+                  "citations.journal",
+                  "references", "referenceCount", "references.authors", "references.year", "references.title",
+                  "references.journal"]
+        URL = semantic_API_url + id + "?fields={fields}".format(
+            fields=",".join(fields))
+        print(URL)
         try:
-            fields = ["citationCount", "authors", "year", "title", "journal", "publicationTypes", "abstract",
-                      "citations", "citations.authors", "citations.year", "citations.title",
-                      "citations.journal",
-                      "references", "referenceCount", "references.authors", "references.year", "references.title", "references.journal"]
-            URL = semantic_API_url + id + "?fields={fields}".format(
-                fields=",".join(fields))
-            print(URL)
             paper = requests.get(URL)
             if paper.status_code == 200:
                 thePaperJSON = paper.json()
@@ -127,8 +122,6 @@ class ImportCitations(forms.Form):
             raise ValidationError("I don't know how to import a paper with more than 1000 references. Not imported.")
         if thePaperJSON["citationCount"] > 1000:
             raise ValidationError("I don't know how to import a paper with more than 1000 citations. Not imported.")
-
-
 
         thePaperObject, created = Paper.objects.get_or_create(SSID_paper_ID=thePaperJSON["paperId"])
         thePaperObject.raw_SS_json = json.dumps(thePaperJSON)
@@ -158,6 +151,5 @@ class ImportCitations(forms.Form):
         except TypeError:
             raise ValidationError("Got a type error when trying to parse the JSON")
         thePaperObject.year = thePaperJSON["year"]
-        thePaperObject.citations_last_queried = datetime.datetime.now().astimezone()
         thePaperObject.save()
-        self.addAuthors(thePaperObject, thePaperJSON["authors"])
+        add_authors(thePaperObject, thePaperJSON["authors"])
